708b5bd6978d574e55b22c47586ee33fe704c0bf
MHD22 <mhd.sad22@gmail.com>
2020-10-26 09:51:01 +0200

    set the structure of html for doctors page

---
+++ js/doctors.js
@@ -0,0 +1,15 @@
+'use strict';
+var container = document.getElementById('doctors-container');
+//info-container class to div  ==> .setAttribute('class', 'info-container')
+//"my-star fas fa-star" classes to i ==> .setAttribute('class', 'my-star fas fa-star');
+
+
+// array of doctors [4];
+
+
+
+//render doctors function()
+//loop
+//create div  parent 
+//create p i   ==> p textContent= array[i]
+
